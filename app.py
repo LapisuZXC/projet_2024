a26bb291a632b8fb029882e3bda94a56b10760fb
Lapisu <dmitriiycmo@gmail.com>
2024-04-26 04:12:02 +0300

    added realtime update of number of lines

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ import platform
 
 class TextLineNumbers(ctk.CTkCanvas):
     def __init__(self,*args,**kwargs):
-        ctk.CTkCanvas.__init__(self,*args,**kwargs)
+        ctk.CTkCanvas.__init__(self,*args,**kwargs) #init using parent class initialisation
         self.textwidget = None
         self.fontSize = 12
         self.configFont()
-
-    def configFont(self):
+        self.text_color = "black"
+    def configFont(self): # dont know if needed tbh
         system = platform.system().lower()
         if system == "windows":
             self.font = font.Font(family='monospace', size=self.fontSize)
@@ -21,24 +21,58 @@ class TextLineNumbers(ctk.CTkCanvas):
             self.font = font.Font(family='monospace',size = self.fontSize)
 
     def attach(self, text_widget):
-        self.textwidget=text_widget
+        self.textwidget = text_widget
 
     def redraw(self,*args):
         self.delete('all')
-        i = self.text_widget.index("@0.0")
+        i = self.textwidget.index("@0,0")
         while True :
-            dline = self.text_widget.dlineinfo(i)
+            dline = self.textwidget.dlineinfo(i)
             if dline is None:
                 break
             y = dline[1]
             linenum = str(i).split(".")[0]
-            self.create_text(1,y,anchor="nw", font=self.font, text=linenum, fill = "pink")
-            i = self.textwidget.index(f"{i}+1line")
+            self.create_text(1,y,anchor="nw", font=self.font,text = linenum, fill = "#003200")
+            i = self.textwidget.index("%s+1line" % i)
 
+class TextPad(tk.Text):
+    
+    def __init__(self, *args, **kwargs):
+        tk.Text.__init__(self, *args, **kwargs, bg="#331e36",fg='white',font=font.Font(family='monospace',size=14))
+        # !!!!
+        self.tk.eval('''
+            proc widget_proxy {widget widget_command args} {
+
+                # call the real tk widget command with the real args
+                set result [uplevel [linsert $args 0 $widget_command]]
+
+                # generate the event for certain types of commands
+                if {([lindex $args 0] in {insert replace delete}) ||
+                    ([lrange $args 0 2] == {mark set insert}) || 
+                    ([lrange $args 0 1] == {xview moveto}) ||
+                    ([lrange $args 0 1] == {xview scroll}) ||
+                    ([lrange $args 0 1] == {yview moveto}) ||
+                    ([lrange $args 0 1] == {yview scroll})} {
+
+                    event generate  $widget <<Change>> -when tail
+                }
+
+                # return the result from the real widget command
+                return $result
+            }
+            ''')
+        self.tk.eval('''
+            rename {widget} _{widget}
+            interp alias {{}} ::{widget} {{}} widget_proxy {widget} _{widget}
+        '''.format(widget=str(self)))
+        # all of that for real time update of TextLineNumbers
+        #basically there is custom tkinted event <<Change>> created 
+
+        self.fontSize = 20
 
 class App(ctk.CTkFrame):
     def __init__(self,master=None):
-        super().__init__(master)
+        super().__init__(master)# again init using parent method    
         self.pack(expand=True,fill=tk.BOTH)
         self.initUI()
         self.style=ttk.Style()
@@ -50,7 +84,7 @@ class App(ctk.CTkFrame):
         
         #texpad
 
-        self.textpad = ctk.CTkTextbox(frame1, text_color="#FFFFFF", bg_color="#3c003f",fg_color="#4c004f")
+        self.textpad = TextPad(frame1)
         self.textpad.pack(fill=tk.BOTH, expand=True)
         
 
@@ -58,12 +92,18 @@ class App(ctk.CTkFrame):
         #TextLineNumbers
         self.textline = TextLineNumbers(frame1, width=30)
         self.textline.attach(self.textpad)
-        self.textline.pack(side='left', fill='y')
+        self.textline.pack(side='left', fill='y',before=self.textpad)
         self.textpad.bind("<<Change>>", self.on_change)
+        self.textpad.bind("<Configure>", self.on_change)
+        
     def on_change(self, event):
         self.textline.redraw()
-app = App()
-app.master.title("temp")
-app.master.minsize(width = 800, height=600)
-app.mainloop()
+
+
+
+if __name__ == '__main__':
+    app = App()
+    app.master.title("temp")
+    app.master.minsize(width = 800, height=600)
+    app.mainloop()
 
